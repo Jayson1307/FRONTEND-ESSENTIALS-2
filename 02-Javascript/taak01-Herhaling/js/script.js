eb0e317b137fde3ff023e5a7efb16794f5606105
Jayson1307 <2125083@talnet.nl>
2021-05-17 11:03:46 +0200

    taak 1 javascript

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,18 @@
 let strNaam;
+strNaam = prompt("hoe heet je?")
+//
 let boolNaamKlopt;
+boolNaamKlopt = confirm("heet je echt " + strNaam)
+if (boolNaamKlopt == true) {
+    alert("welkom " + strNaam + "!")
+}
+else  {
+    strNaam = prompt("okey dan proberen we het even opnieuw")
+}
+boolNaamKlopt = confirm("is " + strNaam + " wel je naam?")
+if (boolNaamKlopt == true){
+    alert("okey, welkom " + strNaam + "!")
+}
+else {
+    alert("okey, evengoed welkom!")
+}
