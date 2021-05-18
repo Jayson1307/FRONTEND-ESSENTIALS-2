c71ad56a75f207d7d5e713a6def0f5c96579e93f
Jayson1307 <2125083@talnet.nl>
2021-05-18 10:35:36 +0200

    taak 3 javascript

--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,9 @@
 function geefRandomGetalTerug(){                
-    
+    return Math.floor((Math.random() * 10) + 1);
 }
 
-console.log(geefRandomGetalTerug());    
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug()); 
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
