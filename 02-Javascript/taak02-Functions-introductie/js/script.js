1789a8d5bfbfd242fa78d8ea47288269c13be3bf
Jayson1307 <2125083@talnet.nl>
2021-05-18 10:22:39 +0200

    taak 2 javascript

---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,22 @@
+function stink(){
+    alert("je stinkt")
+}
+stink();
+let antwoord
+function a(){
+    antwoord = prompt("hoe gaat het met je?")
+}
+a()
+console.log("het gaat " + antwoord)
+let OK
+function b(){
+    OK = confirm("Wilt u de website betreden?")
+}
+b()
+if(b == true){
+    console.log("OK")
+}
+else{
+ console.log("okey dan niet")
+}
+
