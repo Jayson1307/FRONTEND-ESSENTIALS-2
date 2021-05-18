3f1095d49d60e2b5603458ec92edb6c7388347d9
Jayson1307 <2125083@talnet.nl>
2021-05-18 11:50:49 +0200

    blauw rood groen

--- 02-Javascript/taak05-Functions-onClick/js/script.js
+++ 02-Javascript/taak05-Functions-onClick/js/script.js
@@ -7,8 +7,18 @@ function toonAlert(){
 function maakGroen(){
     document.querySelector("body").style.backgroundColor = "Green";
 }
+function maakBlauw(kleur){
+    document.querySelector("body").style.backgroundColor = "blue";
+    //TODO: verander de achtergrond kleur op basis van de meegegeven waarde in de kleur parameter
+}
+
+function maakRood(kleur){
+    document.querySelector("body").style.backgroundColor = "red";
+    //TODO: verander de achtergrond kleur op basis van de meegegeven waarde in de kleur parameter
+}
 
 //TODO: maak 2 functies genaamd maakBlauw en maakRood
+
 
 
 function veranderKleur(kleur){
