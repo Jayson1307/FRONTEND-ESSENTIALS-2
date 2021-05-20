f5f1450495af8a922d02ebf2cc5bcffe6f4f58fd
Jayson1307 <2125083@talnet.nl>
2021-05-20 11:30:45 +0200

    taak 7 js

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,16 @@
+films = ["alita", "detective pikachu", "Avengers Endgame"]
+games = ["Rainbow six siege", "Ark surival evolved", "minecraft"]
+artiesten = ["Juice wrld", "Post malone", "eminem"]
+console.log ("top 3 films:")
+console.log (films[0])
+console.log (films[1])
+console.log (films[2])
+console.log ("top 3 games:")
+console.log (games[0])
+console.log (games[1])  
+console.log (games[2])
+console.log ("top 3 artiesten:")
+console.log (artiesten[0])
+console.log (artiesten[1])
+console.log (artiesten[2])
+
