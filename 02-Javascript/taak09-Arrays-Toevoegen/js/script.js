8210168abab6324980574b9b2a5f48b6674bd293
Jayson1307 <2125083@talnet.nl>
2021-05-25 10:18:40 +0200

    taak 9 klaar js

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -1,3 +1,6 @@
+var nummer = 1;
+var text = "kies film voor plek";
+
 topFilms = [
     "The Godfather",
     "The Shawshank Redemption",
@@ -18,6 +21,29 @@ for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
 }
 
+function checknummer(){
+document.getElementById("nummer").innerHTML = nummer;
+document.getElementById("text").innerHTML = text;
+}
+checknummer()
+
 function kiesFilm(){
-    eerst = prompt("welke film moet er op de eerste plek staan?")
+    film1 = prompt("Wat is je favorite film die je nog niet gekozen hebt?");
+    film1 --;
+    if(film1 < 11){
+        eigenTopFilms.push(topFilms[film1]);
+    nummer++;
+    }
+    film1 ++;
+    document.querySelector(".imdb-film"+film1).style.color = "white";
+    if(nummer >= 4){
+        nummer= "klaar";
+        text= " ";
+    }
+    console.log(eigenTopFilms);
+
+    for(x = 0; x < eigenTopFilms.length; x++){
+        document.querySelector(".eigen-film" + (x+1)).innerHTML = eigenTopFilms[x];
+    }
+    checknummer()
 }
